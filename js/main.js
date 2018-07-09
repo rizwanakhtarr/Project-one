552e5623622930ad5ac82b9b730d1b381363c1ab
unknown <rakhtar@spartaglobal.com>
2018-07-09 22:20:24 +0100

    added timer 30secs

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ var click1;
 var click2;
 var userInput = [];
 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var thirtySeconds = 60 * .5,
+        display = $('.timer');
+    startTimer(thirtySeconds, display);
+});
 
 
  $(".cards").addClass("selected");
@@ -26,7 +48,7 @@ var userInput = [];
       if(click1===click2) {
         console.log("SNAP!");
 
-        
+
       } else {
         console.log("NOT SNAP!");
 
@@ -39,9 +61,11 @@ var userInput = [];
 
 
 
-$(".reset").click(function() {
+  $(".reset").click(function() {
   location.reload();
-});
+
+  });
+
 
 
 });
