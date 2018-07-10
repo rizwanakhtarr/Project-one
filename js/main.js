14583d3c56dcb9d5109a1aa24f8e934f0c4c49a6
unknown <rakhtar@spartaglobal.com>
2018-07-10 22:05:32 +0100

    altered conditions and made match and changed colors

--- js/main.js
+++ js/main.js
@@ -3,95 +3,87 @@ $(document).ready(function(){
 var clicks = 1;
 var click1;
 var click2;
-var userInput = [];
+var modal = $("#simpleModal")[0];
+var modalBtn = $("#modalBtn")[0];
+var closeBtn = $(".closeBtn")[0];
+var winningCondition = 0;
+var sec = 30
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+var timer = setInterval(function() {
+   $('#Timer span').text(sec--);
+   if (sec == -2) {
+     alert("better luck next time!");
+      $('#Timer').fadeOut('fast');
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+      clearInterval(timer);
+   }
+}, 1000);
 
-        display.text(minutes + ":" + seconds);
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-jQuery(function ($) {
-    var thirtySeconds = 60 * .5,
-        display = $('.timer');
-    startTimer(thirtySeconds, display);
-});
-
-
- $(".cards").addClass("selected");
-   $(".cards").click(function(event){
-
-     if(clicks == 1) {
-       $(this).removeClass("selected");
-
-       click1 = event.target.children[0].src;
-       console.log(click1);
+$(".cards").addClass("selected");
+  $(".cards").click(function(event){
+    if(clicks == 1) {
+      $(this).removeClass("selected");
+       click1 = $(this).attr('class').split(' ')[1];
        clicks++;
-    } else if(clicks == 2) {
+
+     } else if(clicks == 2) {
       $(this).removeClass("selected")
-      click2 = event.target.children[0].src;
-      console.log("click1 = "+click1);
-      console.log("click after target"+click2);
+      click2 = $(this).attr('class').split(' ')[1];
       clicks = 1;
 
-      if(click1===click2) {
-        console.log("SNAP!");
+        if(click1===click2) {
+          winningCondition++;
 
+          console.log(winningCondition);
+          setTimeout(function(){
+             $("."+click1).remove();
+          },500);
+
+          if(winningCondition == 8) {
+            setTimeout(function(){
+              modal.style.display = "block";
+            },500)
+
+          }
 
       } else {
-        console.log("NOT SNAP!");
+
+          setTimeout(function(){
+            $("."+click1).addClass("selected");
+            $("."+click2).addClass("selected");
+          },500);
+
+          console.log("not the right match");
 
       }
 
     }
-    console.log(clicks);
     return clicks;
   });
 
 
 
-  $(".reset").click(function() {
+
+  $(".reset").click(function(){
+
   location.reload();
 
   });
 
 
 
+
+$(closeBtn).on("click", closeModal);
+$(modalBtn).on("click", openModal);
+  function closeModal() {
+
+    modal.style.display = "none";
+  }
+
+  function openModal() {
+
+    modal.style.display = "block";
+  }
+
+
 });
-
-
-
-
-
-
-
-// As a user you will able to see 16 cards on screen when click on the 'Start again ' button.
-
-    //create 16 divs in HTML with unique classes
-
-
-
-
-
-// As a User if the correct match clicked, both will remove off deck and if incorrect, the cards will flip back
-
-  //need to create loop through all cards
-  //add click event listener
-  //two classes of cards need to match
-  // create if statement that if selected cards match remove off deck else stay in place and flip back
-
-
-//As a user, on the first page will be displayed title, instructions to the game and start game
-    // need to make instructions to the game in HTML and style with CSS also title and instructions
-    // need to create a HTML button which links to page with game itself.
